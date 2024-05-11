9b8df8ebe8a1b676972bb688dfd229b4ccebbd8f
Vlad Suhankov <vladsuhankov@gmail.com>
2024-05-11 20:23:25 +0700

    fix: Исправил фильтрацию по id кредита и клиентскому id

--- src/InDuckTor.Credit.Feature/Feature/Loan/GetLoanInfoV2.cs
+++ src/InDuckTor.Credit.Feature/Feature/Loan/GetLoanInfoV2.cs
@@ -3,6 +3,7 @@ using InDuckTor.Credit.Domain.LoanManagement;
 using InDuckTor.Credit.Domain.LoanManagement.State;
 using InDuckTor.Credit.Infrastructure.Config.Database;
 using InDuckTor.Shared.Strategies;
+using Microsoft.EntityFrameworkCore;
 
 namespace InDuckTor.Credit.Feature.Feature.Loan;
 
@@ -17,7 +18,9 @@ public class GetLoanInfoV2(LoanDbContext context) : IGetLoanInfoV2
 {
     public async Task<LoanInfoResponse> Execute(GetLoanInfoRequestV2 request, CancellationToken ct)
     {
-        var loan = await context.Loans.FindAsync([request.LoanId], cancellationToken: ct)
+        var loan = await context.Loans
+                       .Where(l => l.Id == request.LoanId && l.ClientId == request.ClientId)
+                       .FirstOrDefaultAsync(ct)
                    ?? throw new Errors.Loan.NotFound(request.ClientId, request.LoanId);
         return LoanInfoResponse.FromLoan(loan);
     }
--- src/InDuckTor.Credit.Feature/Feature/Loan/Payment/GetPaymentInfoV2.cs
+++ src/InDuckTor.Credit.Feature/Feature/Loan/Payment/GetPaymentInfoV2.cs
@@ -2,6 +2,7 @@ using InDuckTor.Credit.Domain.Billing.Payment;
 using InDuckTor.Credit.Domain.Exceptions;
 using InDuckTor.Credit.Infrastructure.Config.Database;
 using InDuckTor.Shared.Strategies;
+using Microsoft.EntityFrameworkCore;
 
 namespace InDuckTor.Credit.Feature.Feature.Loan.Payment;
 
@@ -13,7 +14,9 @@ public class GetPaymentInfoV2(LoanDbContext context, IPaymentRepository paymentR
 {
     public async Task<PaymentInfoResponse> Execute(GetPaymentInfoRequestV2 request, CancellationToken ct)
     {
-        var loan = await context.Loans.FindAsync([request.LoanId, ct], cancellationToken: ct)
+        var loan = await context.Loans
+                       .Where(l => l.Id == request.LoanId && l.ClientId == request.ClientId)
+                       .FirstOrDefaultAsync(ct)
                    ?? throw new Errors.Loan.NotFound(request.ClientId, request.LoanId);
         var undistributedPayments = await paymentRepository.GetAllNonDistributedPayments(loan.Id);
 
